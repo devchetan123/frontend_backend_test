13357a13e0bfae68f3d90aac5c06be5bb77fe278
chetan <profileofchetan123@gmail.com>
2022-06-22 22:16:29 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.get("/pincode", (req, res) => {
     
